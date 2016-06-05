73b757fa14fbb4f2e0c397e6d03cf298b03158e3
bugcode <bugcoding@gmail.com>
2016-06-05 23:24:17 +0800

    add xib custom class

--- CalendarView/CalendarPanel.swift
+++ CalendarView/CalendarPanel.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import Cocoa
+
+class CalendarPanel:NSViewController{
+    
+    var bgColor = NSColor.whiteColor()
+    var generalDateColor = NSColor.grayColor()
+    var holidayDateColor = NSColor.redColor()
+    var selectedDateColor = NSColor.greenColor()
+    var todayDateColor = NSColor.yellowColor()
+    
+    
+    init(){
+        super.init(nibName: "CalendarPanel", bundle: nil)!
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder:coder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // init some self view components
+    func viewInit(){
+        
+    }
+    
+    
+}
