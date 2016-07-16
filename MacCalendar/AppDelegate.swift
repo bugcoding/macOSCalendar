bb39b13a5d234aa42f9a689709bae11959876f8d
bugcode <bugcoding@gmail.com>
2016-07-16 10:51:23 +0800

    rebuild calendar proj

---
+++ MacCalendar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MacCalendar
+//
+//  Created by bugcode on 16/7/16.
+//  Copyright © 2016年 bugcode. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacCalendar/CalendarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalendarViewController.swift
+//  MacCalendar
+//
+//  Created by bugcode on 16/7/16.
+//  Copyright © 2016年 bugcode. All rights reserved.
+//
+
+import Cocoa
+
+class CalendarViewController: NSWindowController {
+    
+    
+    // MARK Outlets define
+    @IBOutlet weak var selectDataText:NSTextField!
+    @IBOutlet weak var leftArrowBtn:NSButton!
+    @IBOutlet weak var rightArrowBtn:NSButton!
+    
+    
+    
+    
+    
+    
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
