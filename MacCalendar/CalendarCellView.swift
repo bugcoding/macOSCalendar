55a871aaa89fb7da7b1ee4528f6cf1bc278c03db
bugcode <bugcoding@gmail.com>
2016-08-17 22:51:55 +0800

    add custom cell view

---
+++ MacCalendar/CalendarCellView.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarCellView.swift
+//  MacCalendar
+//
+//  Created by bugcode on 16/8/17.
+//  Copyright © 2016年 bugcode. All rights reserved.
+//
+
+import Cocoa
+
+class CalendarCellView : NSButton{
+    var cellID:Int = 0
+    
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+    }
+}
