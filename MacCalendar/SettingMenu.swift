b27d08395ccabcf0b01ce8b28d16b263f59dcc51
bugcode <bugcoding@gmail.com>
2016-12-11 15:59:35 +0800

    修改关于页面

--- MacCalendar/SettingMenu.swift
+++ MacCalendar/SettingMenu.swift
@@ -12,7 +12,7 @@ class SettingMenu: NSMenu {
 
     init() {
         super.init(title: "Setting")
-        let set = self.insertItem(withTitle: "登陆启动", action: #selector(SettingMenu.setting(_:)), keyEquivalent: "", at: 0)
+        let set = self.insertItem(withTitle: "设置", action: #selector(SettingMenu.setting(_:)), keyEquivalent: "", at: 0)
         set.target = self
         let ab = self.insertItem(withTitle: "关于", action: #selector(SettingMenu.about(_:)), keyEquivalent: "", at: 1)
         ab.target = self
@@ -32,11 +32,9 @@ class SettingMenu: NSMenu {
         let style = NSMutableParagraphStyle()
         style.alignment = .center
         
-        let url = NSURL(string: "http://bugcode.net")
+        let abbr = [ NSForegroundColorAttributeName: NSColor.black , NSParagraphStyleAttributeName : style, NSFontAttributeName : NSFont.systemFont(ofSize: 11.0)]
         
-        let abbr = [NSParagraphStyleAttributeName : style, NSFontAttributeName : NSFont.systemFont(ofSize: 11.0), NSLinkAttributeName : url]
-        
-        let infoAttributedStr = NSAttributedString(string: "http://bugcode.net", attributes: abbr)
+        let infoAttributedStr = NSAttributedString(string: "report: bugcoding@hotmail.com", attributes: abbr)
         
         NSApp.orderFrontStandardAboutPanel(options: ["Credits":infoAttributedStr])
     }
