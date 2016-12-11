5dab22405ccd3634cc9bbaf431f5e57f37bfa6b8
bugcode <bugcoding@gmail.com>
2016-12-11 23:52:24 +0800

    状态栏图标实时更新

--- MacCalendar/IconView.swift
+++ MacCalendar/IconView.swift
@@ -10,21 +10,15 @@
 import Foundation
 import Cocoa
 
-extension NSImage {
-    convenience init(color: NSColor, size: NSSize) {
-        self.init(size: size)
-        lockFocus()
-        color.drawSwatch(in: NSRect(origin: .zero, size: size))
-        unlockFocus()
-    }
-}
-
 
 class IconView : NSView
 {
     private(set) var image: NSImage
     private let item: NSStatusItem
     
+    var mCurWeek: Int = 0
+    var timer: Timer?
+    var isDraw: Bool = false
     var onMouseDown: () -> ()
     
     var isSelected: Bool {
@@ -38,34 +32,36 @@ class IconView : NSView
     }
     
     // 在image上添加文本
-    func addTextToImage(image: inout NSImage, day: String, week: String) {
-        
+    func addTextToImage() {
+        let util = CalendarUtils.sharedInstance
+        let (year, month, _day) = util.getYMDTuppleBy(util.getDateStringOfToday())
+        let _week = util.getWeekDayBy(year, month: month, day: _day)
+        let day = String(_day)
+        let week = String(CalendarConstant.WEEK_SHORT_CUT[_week])
+        mCurWeek = _week
         let style = NSMutableParagraphStyle()
         style.alignment = .center
-        
+        let size = self.image.size
+
         // 日期文本
         let dayAttrDict = [NSForegroundColorAttributeName : NSColor.black, NSParagraphStyleAttributeName : style, NSFontAttributeName : NSFont.systemFont(ofSize: 9.8)]
-        image.lockFocus()
         let dayString = NSString(string: day)
-        var pos = NSPoint(x: image.size.width / 2 - dayString.size(withAttributes: dayAttrDict).width / 2, y: 0.5)
-        dayString.draw(at: pos, withAttributes: dayAttrDict)
+        var rect = CGRect(x: 2, y: 2, width: size.width, height: size.height - 5.5)
+
+        dayString.draw(in: rect, withAttributes: dayAttrDict)
         
         // 星期文字
         let weekAttrDict = [NSForegroundColorAttributeName : NSColor.white, NSParagraphStyleAttributeName : style, NSFontAttributeName : NSFont.systemFont(ofSize: 3.8)]
-        let weekString = NSString(string: week)
-        pos = NSPoint(x: image.size.width / 2 - weekString.size(withAttributes: weekAttrDict).width / 2, y: 12.5)
-        weekString.draw(at: pos, withAttributes: weekAttrDict)
-        
-        image.unlockFocus()
+        let weekString = NSString(string: week!)
+        rect = CGRect(x: 2, y: 2.5, width: size.width, height: size.height)
+        weekString.draw(in: rect , withAttributes: weekAttrDict)
     }
     
     
     init(imageName: String, item: NSStatusItem) {
         let thickness = NSStatusBar.system().thickness
 
-        var image = NSImage(named: imageName)!
-        
-        self.image = image
+        self.image = NSImage(named: imageName)!
         self.item = item
         self.item.highlightMode = true
         self.isSelected = false
@@ -75,28 +71,38 @@ class IconView : NSView
         
         super.init(frame: rect)
         
-        let util = CalendarUtils.sharedInstance
-        let (year, month, day) = util.getYMDTuppleBy(util.getDateStringOfToday())
-        let week = util.getWeekDayBy(year, month: month, day: day)
-        
         // 图标上加入文本
-        addTextToImage(image: &image, day: String(day), week: String(CalendarConstant.WEEK_SHORT_CUT[week]))
+        addTextToImage()
         
-
+        // 启用计时器，控制每秒执行一次tickDown方法
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(IconView.reDraw), userInfo: nil, repeats: true)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func reDraw() {
+        let util = CalendarUtils.sharedInstance
+        let (year, month, day) = util.getYMDTuppleBy(util.getDateStringOfToday())
+        let week = util.getWeekDayBy(year, month: month, day: day)
+        if mCurWeek != week {
+            mCurWeek = week
+            addTextToImage()
+            self.needsDisplay = true
+        }
+        
+    }
+    
     override func draw(_ dirtyRect: NSRect) {
         self.item.drawStatusBarBackground(in: dirtyRect, withHighlight: self.isSelected)
         
-        self.item.title = "Cal"
         let size = self.image.size
         let rect = CGRect(x: 2, y: 2, width: size.width, height: size.height)
-        
-        self.image.draw(in: rect)
+        self.image.draw(in: rect, from: NSRect.zero, operation: NSCompositeSourceOver, fraction: 1.0)
+
+        // 图标上加入文本
+        addTextToImage()
     }
     
     override func mouseDown(with event: NSEvent) {
