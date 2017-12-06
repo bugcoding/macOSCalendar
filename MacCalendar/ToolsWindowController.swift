662c3de71779041d99848cf39c13f08f1dde84c1
bugcode <bugcoding@gmail.com>
2017-12-06 23:15:34 +0800

    添加公历农历转换逻辑

--- MacCalendar/ToolsWindowController.swift
+++ MacCalendar/ToolsWindowController.swift
@@ -16,8 +16,18 @@ class ToolsWindowController : NSWindowController, NSWindowDelegate {
     // 转换按钮
     @IBOutlet weak var transButton: NSButton!
     // 当前是哪种模式
-    private var mode:Int8 = 0;
+    private var mode:Int8 = -1;
+    // 公历详情
     @IBOutlet weak var goriDetail: NSTextField!
+    // 农历详情
+    @IBOutlet weak var lunarDetail: NSTextField!
+    
+    // 年月日选择
+    @IBOutlet weak var yearSelect: NSPopUpButton!
+    @IBOutlet weak var monthSelect: NSPopUpButton!
+    @IBOutlet weak var daySelect: NSPopUpButton!
+    
+    
     
     // 农历转公历选中
     @IBAction func lunarOrGoriSelected(_ sender: NSButton) {
@@ -26,6 +36,8 @@ class ToolsWindowController : NSWindowController, NSWindowDelegate {
         } else {
             mode = 0
         }
+        
+        addPopUpItem()
         print("mode == \(mode)")
     }
     
@@ -36,11 +48,48 @@ class ToolsWindowController : NSWindowController, NSWindowDelegate {
         return "ToolsWindowController"
     }
     
+    // 初始化公历日期与月份
+    func initGoriPopupItems() {
+        monthSelect.removeAllItems()
+        for i in 1 ... 12 {
+            monthSelect.addItem(withTitle: "\(i) 月")
+        }
+        daySelect.removeAllItems()
+        for i in 1 ... 31 {
+            daySelect.addItem(withTitle: "\(i) 日")
+        }
+    }
+    // 初始化农历日期
+    func initLunarPopupItems() {
+        monthSelect.removeAllItems()
+        for i in CalendarConstant.nameOfChnMonth {
+            monthSelect.addItem(withTitle: i + "月")
+        }
+        daySelect.removeAllItems()
+        for i in CalendarConstant.nameOfChnDay {
+            daySelect.addItem(withTitle: i)
+        }
+    }
+    
+    // 添加年月日待选项
+    func addPopUpItem() {
+        yearSelect.removeAllItems()
+        for i in 1900 ... 2100 {
+            yearSelect.addItem(withTitle: "\(i)")
+        }
+        if mode == 1 {
+            initGoriPopupItems()
+        } else if mode == 0 {
+            initLunarPopupItems()
+        }
+    }
+    
     override func windowDidLoad() {
         super.windowDidLoad()
         gori2Lunar.state = 1
         lunar2Gori.state = 0
         goriDetail.alignment = .center
+        addPopUpItem()
     }
     
 }
