bdf9f6d4fc9b21eeb43335d11ae5aeb95816ce37
bugcode <bugcoding@gmail.com>
2019-08-08 11:16:38 +0800

    icon更换，状态栏图标更换.

---
+++ MacCalendar/NibLoadable.swift
@@ -0,0 +1,19 @@
+//
+//  NibLoadable.swift
+//  StatebarCalendar
+//
+//  Created by bugcode on 2019/8/6.
+//  Copyright © 2019 bugcode. All rights reserved.
+//
+
+import Cocoa
+
+class NibLoadable: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
---
+++ MacCalendar/StatusBarView.swift
@@ -0,0 +1,19 @@
+//
+//  StatusBarView.swift
+//  StatebarCalendar
+//
+//  Created by bugcode on 2019/8/3.
+//  Copyright © 2019 bugcode. All rights reserved.
+//
+
+import Cocoa
+
+class StatusBarView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
